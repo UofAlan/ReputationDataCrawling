3269f932d92368aa9c6a834d01deb1ce219f0801
Alan <alan66sqa@gmail.com>
2017-08-28 09:09:58 -0700

    Update crawlingController.java

--- githubCrawling/src/main/java/com/alan/github/controllers/crawlingController.java
+++ githubCrawling/src/main/java/com/alan/github/controllers/crawlingController.java
@@ -4,14 +4,18 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.util.UriComponentsBuilder;
 
 import com.alan.github.model.GitInfo;
 import com.alan.github.repostories.GitInfoRepository;
@@ -33,6 +37,19 @@ class crawlingController{
 	public List<GitInfo> getGitInfo(){
 		List<GitInfo> list = gRepository.findAll();
 		return list;
+	}
+	//POST
+	@PostMapping("/info/add")
+	public ResponseEntity<Void> createUser(@RequestBody GitInfo info, UriComponentsBuilder ucBuilder){ 
+	if(gRepository.findByName(info.getName())==null) {
+		gRepository.save(info);
+		HttpHeaders headers = new HttpHeaders();
+	    headers.setLocation(ucBuilder.path("/info/added={name}").buildAndExpand(info.getName()).toUri());
+	    return new ResponseEntity<Void>(headers, HttpStatus.CREATED);
+	}
+	else {
+		return new ResponseEntity<Void>(HttpStatus.CONFLICT);
+	}
 	}
 	@RequestMapping("/info/name={name}")
 	public GitInfo getInfoByName( @PathVariable String name) {
@@ -74,15 +91,7 @@ class crawlingController{
 //		return list;
 //		
 //	}
-	//POST
-	@PostMapping("/info/add={name}/{author}")
-	public void save(@PathVariable("name") String name, @PathVariable("author") String author) {
-		GitInfo info1 = new GitInfo();
-		info1.setAuthor(author);
-		info1.setName(name);
-		
-		gRepository.save(info1);
-	}
+
 	//PATCH
 	@PatchMapping("/info/patch={name}/{author}")
 	public void modify(@PathVariable("name") String name, @PathVariable("author") String author) {
